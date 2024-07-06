358d2d4ea8653af825e815804720e715a055b5f4
code-iddih <idinyamwaya@gmail.com>
2024-07-06 23:24:53 +0300

    Adds DOMContentLoaded Event

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+  
+    // Target the paragraph element with id="text" and replace its text content
+    const textParagraph = document.getElementById("text");
+    if (textParagraph) {
+      textParagraph.textContent = "This is really cool!";
+    }
+  });
+  
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
+  
